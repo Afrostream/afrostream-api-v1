3cccaa065386442f36587fd35bac3524a80fecdc
John Archambault <jarchambault@libertyglobal.com>
2015-07-14 02:56:29 +0200

    ajoute lenvironnement du staging dans le fichier configs

---
+++ config/environment/staging.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+	// auth0 details for connecting to dev environment
+
+	auth0: {
+		domain: 'johnarch.auth0.com',
+		clientId: 'hMshepR8jkfbsgjUBxZ2o06M9euIneRD',
+		clientSecret: '95Bs6ObYccAZ3Kt9aFhGiEham_8boU4XN8Ly-vJC0ISvUBZ39Ah3phP26ha7jS1n'
+	},
+	// MongoDB connection options
+	mongo: {
+		uri: 'mongodb://afrostream:afr0str3am@ds047712.mongolab.com:47712/heroku_nd0hbtmt'
+	},
+	seedDB: true
+};
