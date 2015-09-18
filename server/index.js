fa7255e07e3bb5a76aa3a51fec591a8dda2e054f
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-09-18 12:56:24 +0200

    rootRequire()

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 'use strict';
 
+// permet de require un fichier depuis la racine du projet
+// usage ex: rootRequire('/server/config/environment/');
+global.rootRequire = function (name) { return require(__dirname + '/../' + name); };
+global.__base = __dirname + '/../';
+
 // Export the application
 exports = module.exports = require('./app');
