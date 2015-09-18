ffa71012fa337894e68fc2ad76c06d42c3c3f3e7
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-09-18 13:03:20 +0200

    refactor: the configuration should be in a unique place /server/config/environment/*

--- server/components/purest/provider/afrostream.js
+++ server/components/purest/provider/afrostream.js
@@ -1,7 +1,7 @@
 var Purest = require('purest')
   , async = require('async')
   , _ = require('lodash')
-  , config = require('../../../config/environment');
+  , config = rootRequire('/server/config/environment');
 
 function Afrostream() {
   this.client = new Purest({
@@ -9,7 +9,7 @@ function Afrostream() {
     token: '[ACCESS_TOKEN]',
     key: config.afrostream.apiKey,
     secret: config.afrostream.apiSecret,
-    config: require('../config/afrostream.provider.json')
+    config: config.purest.providers
   });
 }
 
