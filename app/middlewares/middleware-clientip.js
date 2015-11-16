52ec9e50301d64e00ed203e84563cf8f88236725
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-16 16:25:05 +0100

    fix clientip

--- app/middlewares/middleware-clientip.js
+++ app/middlewares/middleware-clientip.js
@@ -10,15 +10,12 @@
  */
 module.exports = function (options) {
   return function (req, res, next) {
-    if (req.get('x-forwarded-for')) {
-      // assuming we are on heroku ... (might be false)
-      //  the client is the first one (left) in the list of x-forwarded-for
-      //  heroku router ip is in req.ip
-      // we trim the result.
-      req.clientip = req.get('fastly-client-ip') || String(req.get('x-forwarded-for').split(',').shift() || req.ip).replace(/^\s+|\s+$/g, '');
-    } else {
-      req.clientip = req.ip;
-    }
+    // assuming we are on heroku ... (might be false)
+    //  the client is the first one (left) in the list of x-forwarded-for
+    //  heroku router ip is in req.ip
+    // we trim the result.
+    req.clientip = req.get('fastly-client-ip') || String(req.get('x-forwarded-for').split(',').shift() || req.ip).replace(/^\s+|\s+$/g, '');
+    //
     next();
   };
 };
