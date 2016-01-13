2bda393181b3855905b5fb756488cb3efe8321fa
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-01-13 11:09:04 +0100

    test: /auth/signup integration tests

--- test/integration/auth.js
+++ test/integration/auth.js
@@ -10,6 +10,68 @@ var request = require('supertest');
 
 var assert = require('better-assert');
 
+describe('POST /auth/signup', function () {
+  before(function (done) {
+    require('request')({
+      method: 'GET',
+      uri: 'http://'+config.backend.authority+'/alive',
+      json: true
+    }, function (err, response, body) {
+      if (err || !body || !body.alive) {
+        done('to run the test, dev backend must be UP & ready.');
+      } else {
+        done();
+      }
+    });
+  });
+
+  var email = 'test.api-v1.'+String(Date.now())+'@afrostream.tv';
+  var accessToken;
+
+  it('should respond 200 OK with a valid access_token', function(done) {
+    request(app)
+      .post('/auth/signup')
+      .send({
+        email: email,
+        password: 'test'
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, function (err, res) {
+        assert(res.body.accessToken);
+
+        accessToken = res.body.accessToken;
+
+        request(app)
+          .get('/api/users/me?afro_token='+accessToken)
+          .expect(200, function (err, res) {
+            assert(res.body.email === email);
+
+            done();
+          });
+      });
+  });
+
+  it('should respond 200 OK with a valid access_token', function(done) {
+    request(app)
+      .post('/auth/signin')
+      .send({
+        email: email,
+        password: 'test'
+      })
+      .expect('Content-Type', /json/)
+      .expect(200, function (err, res) {
+        assert(res.body.accessToken);
+
+        request(app)
+          .get('/api/users/me?afro_token=' + res.body.accessToken)
+          .expect(200, function (err, res) {
+            assert(res.body.email === email);
+            done();
+          });
+      });
+  });
+});
+
 describe('POST /auth/signin', function() {
   before(function (done) {
     require('request')({
@@ -41,7 +103,7 @@ describe('POST /auth/signin', function() {
           .expect(200, function (err, res) {
             assert(res.body.email === 'admin@admin.com');
             done();
-          })
+          });
       });
   });
 });
