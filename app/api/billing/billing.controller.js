22afe990400d2ee9105a00006639c714c467c4ac
benjipott <pott.benjamin@gmail.com>
2016-03-07 09:51:16 +0100

    add gift route

--- app/api/billing/billing.controller.js
+++ app/api/billing/billing.controller.js
@@ -10,6 +10,10 @@ exports.createSubscriptions = function (req, res) {
   backend.postData(req, '/api/billings/subscriptions').nodeify(backend.fwd(res));
 };
 
+exports.createGift = function (req, res) {
+  backend.postData(req, '/api/billings/gift').nodeify(backend.fwd(res));
+};
+
 exports.cancelSubscriptions = function (req, res) {
   backend.putData(req, '/api/billings/subscriptions/' + req.params.subscriptionUuid + '/cancel').nodeify(backend.fwd(res));
 };
