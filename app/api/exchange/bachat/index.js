d2300764fdf0c572020bbb40284aaaa22b45e525
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-04-19 18:00:53 +0200

    bouygues: debug

--- app/api/exchange/bachat/index.js
+++ app/api/exchange/bachat/index.js
@@ -5,6 +5,12 @@ var router = express.Router();
 
 var controller = require('./bachat.controller.js');
 
+// temporary debug
+router.use(function (req, res, next) {
+  console.log('HEADERS: ', req.headers);
+  next();
+});
+
 router.post('/customers', controller.customers);
 router.post('/subscriptions', controller.subscriptions);
 router.post('/refunds', controller.refunds);
