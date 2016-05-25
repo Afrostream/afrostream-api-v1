0f82fcc3e46ab59e891c3bd0cb18feec8bbdc6ad
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-05-25 15:24:23 +0200

    bugfix: config

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@
 
 var config = require('afrostream-node-config');
 
-config.set('development', require('./environment/development.js'));
-config.set('test', require('./environment/test.js'));
-config.set('staging', require('./environment/staging.js'));
-config.set('production', require('./environment/production.js'));
+config.merge('development', require('./environment/development.js'));
+config.merge('test', require('./environment/test.js'));
+config.merge('staging', require('./environment/staging.js'));
+config.merge('production', require('./environment/production.js'));
 
 module.exports = config.get();
