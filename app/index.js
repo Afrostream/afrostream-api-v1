a6568f62961446b7d76b302a67081f0568a38dda
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-08-09 15:19:18 +0200

    feature: https on port 3443 in dev env

--- app/index.js
+++ app/index.js
@@ -7,8 +7,17 @@ var config = require('../config');
 
 var app = require('./app.js');
 
+if (process.env.NODE_ENV === 'development') {
+  var pem = require('pem');
+  pem.createCertificate({days: 1, selfSigned: true}, function (err, data) {
+    require('https').createServer({key: data.serviceKey, cert: data.certificate}, app).listen(3443, config.ip, function () {
+      console.log('Express HTTPS server listening on %d, in %s mode', 3443, app.get('env'));
+    })
+  });
+}
+
 // Start server
 app.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  console.log('Express HTTP server listening on %d, in %s mode', config.port, app.get('env'));
 });
 module.exports = app;
